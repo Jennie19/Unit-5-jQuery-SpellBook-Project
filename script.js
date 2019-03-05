fc156545c146aed2655bef7c058b0c544fcf360f
Jennie19 <43611295+Jennie19@users.noreply.github.com>
2019-03-05 09:38:23 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".B").click(function() {
+    $(".img1").fadeToggle(2000);
+
+});
+
+//Spell #2
+
+$(".C").click(function() {
+    $(".img2").slideToggle(1000);
+ 
+});
+
+
+
+//Spell #3
+
+$(".V").click(function() {
+    $(".img3").toggle(120);
+ 
+});
+
+
